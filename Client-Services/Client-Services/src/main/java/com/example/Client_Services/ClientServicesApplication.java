2067a7378fa818bd20bdc6bd01eb93f55cf876fb
Taha_Essaouir <154090928+Taha-Essaouir@users.noreply.github.com>
2024-11-24 19:21:55 +0100

    waaaaaa

--- Client-Services/Client-Services/src/main/java/com/example/Client_Services/ClientServicesApplication.java
+++ Client-Services/Client-Services/src/main/java/com/example/Client_Services/ClientServicesApplication.java
@@ -9,5 +9,4 @@ public class ClientServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientServicesApplication.class, args);
 	}
-
 }
---
+++ Product-Services/src/main/java/repositories/ProduitRepository.java
@@ -0,0 +1,24 @@
+package repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProduitRepository extends CrudRepository {
+/* l'entité à gérer
+Integer :le type de la clé primaire id int ;>
+
+les méthodes générique:
+save(produit)
+findById(id)
+findAll()
+finByNom(nom)
+findByMarque(marque)
+findByPrixAndMarque(prix,marque)
+findByPrixOrMarque(prix,marque)
+....
+deleteById(id)
+deleteAll()
+...
+*/
+}
